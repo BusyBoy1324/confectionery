aae6b217a304c8219cf3396aa01fc955406768a9
BusyBoy1324 <vanya.sobol.83@gmail.com>
2024-02-03 20:10:24 +0200

    statistic + fixes

---
+++ confectionery-back/ViewModels/PieChartViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace confectionery_back.ViewModels
+{
+    public class PieChartViewModel
+    {
+        public string Product { get; set; }
+        public int Count { get; set; }
+        public double Percent { get; set; }
+    }
+}
